0254a8ff0191e87ef07a5618ffe52f6f0a989f7e
milosz-k <miloszkitowski@gmail.com>
2022-08-27 09:40:34 +0200

    documentation strings

--- car_manager.py
+++ car_manager.py
@@ -7,12 +7,14 @@ MOVE_INCREMENT = 10
 
 
 class CarManager():
-    def __init__(self):
+    """This class is responsible for cars which are passing the road managment."""
+    def __init__(self) -> None:
         super().__init__()
         self.block = []
         self.create_car()
     
-    def create_car(self):
+    def create_car(self) -> None:
+        """This method creates new 2x1 block sized car and turns it to the left."""
         for new_part in range(2):
             new_part = Turtle("square")
             new_part.penup()
@@ -24,7 +26,8 @@ class CarManager():
         self.block[0].right(180)
         self.block[1].right(180)
     
-    def move(self, speed):
+    def move(self, speed) -> None:
+        """This method assigns speed value to the car."""
         for part in self.block:
             part.forward(STARTING_MOVE_DISTANCE + speed * MOVE_INCREMENT)
 
--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,9 @@ FONT = ("Courier", 24, "normal")
 GAMEOVERFONT = ("Courier", 48, "normal")
 
 class Scoreboard(Turtle):
+    """This class is responsible for level management."""
     def __init__(self):
+        """Init level on the screen."""
         super().__init__()
         self.level = 1
         self.color("black")
@@ -14,11 +16,13 @@ class Scoreboard(Turtle):
         self.write(f"Level: {self.level}", align="center", font = FONT)
     
     def increase_level(self):
+        """This method increases current level."""
         self.level += 1
         self.clear()
         self.write(f"Level: {self.level}", align="center", font = FONT)
 
     def game_over(self):
+        """This method prints ending text on the screen."""
         self.clear()
         self.goto(0.00, 0.00)
         self.write(f"GAME OVER", align = "center", font = GAMEOVERFONT)
